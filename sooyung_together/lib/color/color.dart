e57d9c784569a03f4a6512903fc979a6b3b38ba8
sin woo kyoung <sinwookyoung15@gmail.com>
2025-01-30 18:58:39 +0900

    2025-01-30

---
+++ sooyung_together/lib/color/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Colors.white;
+const Color mainColor = Colors.purple;
+const Color backgroundColor = Colors.deepPurpleAccent;
+const Color foregroundColor = Colors.purpleAccent;
--- sooyung_together/lib/main.dart
+++ sooyung_together/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sooyung_together/screen/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
     MaterialApp(
